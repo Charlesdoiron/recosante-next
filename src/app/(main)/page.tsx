c1914942d47ef243228bd53af31a51db23aef881
charlesdoiron <doironcharles@gmail.com>
2024-02-07 15:44:15 +0100

    chore: deploy

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,10 +1,7 @@
 import { CallToAction } from '@/components/CallToAction';
-import { Faqs } from '@/components/Faqs';
 import { Hero } from '@/components/hero';
-import { Pricing } from '@/components/Pricing';
 import { PrimaryFeatures } from '@/components/primary-features';
 import { Reviews } from '@/components/Reviews';
-import { SecondaryFeatures } from '@/components/SecondaryFeatures';
 import { Notification } from '@/components/notifications';
 import { Incentive } from '@/components/incentive';
 
